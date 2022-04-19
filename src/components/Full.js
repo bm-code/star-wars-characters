341afdde195819489f03afeab9cc4cc574aa6f9d
Beni Morales <i.benimorales@gmail.com>
2022-04-19 23:20:50 +0200

    Proyecto finalizado, falta componetizar

--- src/components/Full.js
+++ src/components/Full.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
-import './CharacterList.css'
+import './CharacterList.css';
+import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
+import { IconContext } from 'react-icons';
 
 export default function Full() {
     const API_URL = 'https://swapi.dev/api';
@@ -10,6 +12,7 @@ export default function Full() {
         fetch(`${API_URL}/people/?page=${page}`)
             .then(response => response.json())
             .then(data => setCharacterList(data.results))
+            .catch(error => console.log(`Ha habido un error de conexión: ${error.message}`))
     }, [page]);
 
     const nextPage = () => page >= 1 && page < 9 ? setPage(page + 1) : setPage(page);
@@ -18,17 +21,46 @@ export default function Full() {
 
     function showDetailsToggle(event) {
         event.target.childNodes[1].style.display === 'none' ?
-            event.target.childNodes[1].style.display = 'flex'
+            event.target.childNodes[1].style.display = 'block'
             :
             event.target.childNodes[1].style.display = 'none';
     }
 
+    function orderByName() {
+        setCharacterList([...characterList].sort(function (a, b) {
+            if (a.name > b.name) return 1;
+            if (a.name < b.name) return -1
+            return 0
+        }));
+    }
+
+    function orderByHeight() {
+        setCharacterList([...characterList].sort(function (a, b) {
+            if (a.height > b.height) return 1;
+            if (a.height < b.height) return -1
+            return 0
+        }));
+    }
+
     return (
         <section className="main">
+            <div className="order">
+                <button onClick={() => orderByName()} className="order-btn">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span> Order by name
+                </button>
+                <button onClick={() => orderByHeight()} className="order-btn">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>Order by height
+                </button>
+            </div>
             <ul className="character-list">
                 {characterList.map(character => {
-                    return <li onClick={(event) => showDetailsToggle(event)} className="character-list__item" key={character.name}>
-                        {character.name}
+                    return <li onClick={(event) => showDetailsToggle(event)} className="character-list__item" key={character.name}>{character.name}
                         <ul style={{ display: "none" }} id={character.name} className="character-list__item-details">
                             {character.height === 'n/a' ? '' : <li>Height: {character.height}cm</li>}
                             {character.hair_color === 'n/a' ? '' : <li>Hair color: {character.hair_color}</li>}
@@ -40,8 +72,10 @@ export default function Full() {
 
 
             <div className="pagination">
-                <button onClick={() => previousPage()} className="previous">Previous</button>
-                <button onClick={() => nextPage()} className="next">Next</button>
+                <IconContext.Provider value={{ className: "button-icon" }}>
+                    {page > 1 ? <button onClick={() => previousPage()}><FiArrowLeft /> Previous page</button> : ''}
+                    {page < 9 ? <button onClick={() => nextPage()}>Next page <FiArrowRight /></button> : ''}
+                </IconContext.Provider>
             </div>
         </section >
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,15 @@
-import logo from '../assets/logo.svg'
-import './Header.css'
+import logo from '../assets/logo.png';
+import './Header.css';
 
 export default function Header() {
+
+
     return (
         <header>
             <div className="header-main">
                 <div className="logo">
                     <img src={logo} alt="Start Wars logo" />
                 </div>
-                <nav className="order">
-                    <button className="header-btn name-order">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span> Order by name
-                    </button>
-                    <button className="header-btn height-order">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>Order by height
-                    </button>
-                </nav>
             </div>
         </header>
     )
