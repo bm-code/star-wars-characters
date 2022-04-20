5d3bf18f1da030465320c3b34bf46a9d392de602
Beni Morales <i.benimorales@gmail.com>
2022-04-20 22:00:28 +0200

    Botones activos modificados

--- src/components/Ordernation.js
+++ src/components/Ordernation.js
@@ -3,12 +3,12 @@ export default function Ordernation({ orderType, setOrderType }) {
 
     return (
         <div className="order">
-            <button onClick={() => setOrderType(0)} className="order-btn">
+            <button onClick={() => setOrderType(0)} className={orderType === 0 ? "order-btn active" : "order-btn"}>
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span> Order by name</button>
-            <button onClick={() => setOrderType(1)} className="order-btn">
+            <button onClick={() => setOrderType(1)} className={orderType === 1 ? "order-btn active" : "order-btn"}>
                 <span></span>
                 <span></span>
                 <span></span>
